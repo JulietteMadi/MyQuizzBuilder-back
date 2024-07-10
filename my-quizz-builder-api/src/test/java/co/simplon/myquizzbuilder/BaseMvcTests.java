da3700de8f214f749109c8c2a552b7b8eb225a6d
JulietteMadi <juliette.madinier@gmail.com>
2024-07-10 16:56:09 +0200

    changed path in BaseMvcTests

--- my-quizz-builder-api/src/test/java/co/simplon/myquizzbuilder/BaseMvcTests.java
+++ my-quizz-builder-api/src/test/java/co/simplon/myquizzbuilder/BaseMvcTests.java
@@ -18,7 +18,7 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles(value = "devtests")
+@ActiveProfiles(value = "test")
 @Sql(scripts = {
 	"classpath:db/schema-tests.ddl.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_CLASS)
 @Import(ControllerMock.class)
